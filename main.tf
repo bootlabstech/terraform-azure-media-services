843e9c394e20a719d6395e3aa66b161de671dd19
lalita2407 <lalitalallu02@gmail.com>
2024-02-20 18:03:38 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_media_services_account" "media_services" {
     id         = azurerm_storage_account.sa.id
     is_primary = true
   }
-  depends_on = [ azurerm_user_assigned_identity.example ]
+  depends_on = [ azurerm_user_assigned_identity.user_assigned_identity ]
 }
 resource "azurerm_user_assigned_identity" "user_assigned_identity" {
   location            = var.resource_group_location
